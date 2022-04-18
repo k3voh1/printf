353c7ca39e32b4cd57fa119271eb19a7606d3758
k3voh1 <kelvinduke.kj@gmail.com>
2022-04-18 10:31:49 +0000

    Init commit for task 0 - printf character and string

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string composed of zero
+ * or more directives
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
